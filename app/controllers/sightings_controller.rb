3b183311b4f94453cab1c6e149837a260557c0f1
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-12-10 23:43:27 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -3,6 +3,7 @@ class SightingsController < ApplicationController
     sighting = Sighting.find_by(id: params[:id])
     render json: sighting, include: [:bird, :location]
   end
+  
   def index
     sightings = Sighting.all
     render json: sightings, include: [:bird, :location]
